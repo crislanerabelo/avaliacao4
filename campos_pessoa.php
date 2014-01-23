9833787a8db94dfa9bbbda3da7909ca70cd0c0a2
crislanerabelo <crislane_rabelo@hotmail.com>
2014-01-23 17:58:29 -0200

    Create campos_pessoa.php

---
+++ campos_pessoa.php
@@ -0,0 +1,24 @@
+<label>Nome: </label><input name="nome" type="text" />
+<br/>
+<label>CPF: </label><input name="cpf" type="text" />
+<br/>
+<label>Nascimento: </label><input name="nascimento" type="text" />
+<br/>
+<label>Sexo: </label>&nbsp;
+<input name="sexo" type="radio" value="Masculino" /><label>Masculino</label>
+<input name="sexo" type="radio" value="Feminino" /><label>Feminino</label>
+<br/>
+<label>Estado: </label>
+<select name="estado">
+    <option value="-1">Selecione...</option>
+    <option>Bahia</option>
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+</select>
+<br/>
+<label>Telefone: </label><input name="telefone" type="text" />
+<br/>
+<label>Observações: </label><br/>
+<textarea name="observacoes" rows="7" cols="40"></textarea>
+<br/><br/>
+<input type="submit" value="Enviar" />
